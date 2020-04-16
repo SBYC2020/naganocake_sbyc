5cd9c34d0bc3f15a443aaa5369d2c67533c08114
koharakouki <ko.kohara02@gmail.com>
2020-04-16 02:21:11 +0000

    新規登録ページ追加

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -7,4 +7,16 @@ class Customer < ApplicationRecord
   has_many :orders
   has_many :shipping_addresses
   has_many :carts
+
+  validates :surname, presence: true
+  validates :name, presence: true
+  validates :kana_surname, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :kana_name, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :postal_code, presence: true, format: { with: /\A\d{7}\z/ }
+  validates :address, presence: true
+  validates :phone_number, presence: true, format: { with: /\A\d{10,11}\z/ }
+  VALID_EMAIL_REGIX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+			format: { with: VALID_EMAIL_REGIX }, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6 }
 end
