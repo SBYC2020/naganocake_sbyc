50b2505d620cf10bf836ab7012243f282d657e66
kevin0577 <vagrant@localhost.localdomain>
2020-04-17 02:33:29 +0000

    ヘッダー追加

--- app/helpers/admin/cusotmers_helper.rb
+++
@@ -1,2 +0,0 @@
-module Admin::CusotmersHelper
-end
--- test/controllers/shipping_addresses_controller_test.rb
+++ test/controllers/shipping_addresses_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class Admin::CusotmersControllerTest < ActionDispatch::IntegrationTest
+class ShippingAddressesControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
