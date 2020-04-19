a2f75bc47064219550a1757523ab10b67393834b
shumpei14 <pein14soccer@gmail.com>
2020-04-19 10:00:21 +0000

    マイページを編集しました

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,10 +3,10 @@ class ProductsController < ApplicationController
 		@products = Product.all
 	end
 	def show
-		@products = Product.find(params[:id])
+		@product = Product.find(params[:id])
 	end
 	private
 	def products_params
-		params.require(:products).permit(:image_id, :product_name, :tax_included_price)
+		params.require(:products).permit(:image_id, :product_name, :tax_included_price, :product_description)
 	end
 end
